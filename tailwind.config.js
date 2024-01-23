8c95de293c588e56b3a82494f04b0c013723de5b
Stephen Muñez <sjtmunez@gmail.com>
2024-01-23 13:11:27 +0800

    setup: fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: '#9e7f66',
+        'light-violet': '#939BF4',
+        'very-dark-blue': '#19202D',
+        midnight: '#121721',
+        white: '#FFFFFF',
+        'light-grey': '#F4F6F8',
+        'dark-grey': '#6E8098',
+        gray: '#9DAEC2',
+      },
+      fontFamily: {
+        sans: ['"Kumbh Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
