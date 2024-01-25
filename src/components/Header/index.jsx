049efd6f68c47ab68f0dfeb40927654ef43ae566
Stephen Muñez <sjtmunez@gmail.com>
2024-01-25 12:38:36 +0800

    feat: header mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,10 +3,19 @@ import bgPatternMobile from "/assets/mobile/bg-pattern-header.svg";
 import bgPatternTablet from "/assets/tablet/bg-pattern-header.svg";
 import bgPatternDesktop from "/assets/desktop/bg-pattern-header.svg";
 import logo from "/assets/desktop/logo.svg";
+import sunIcon from "/assets/desktop/icon-sun.svg";
+import moonIcon from "/assets/desktop/icon-moon.svg";
 
 const Header = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [backgroundPattern, setBackgroundPattern] = useState("");
+
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+    console.log(isDarkMode);
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -33,12 +42,38 @@ const Header = () => {
 
   return (
     <header
-      className="bg-cover bg-no-repeat px-6 pb-[4.5rem] pt-8"
+      className="flex items-center justify-between bg-cover bg-no-repeat px-6 pb-[4.5rem] pt-8"
       style={{ backgroundImage: `url(${backgroundPattern})` }}
     >
       <a href="/">
         <img src={logo} alt="devjobs logo" />
       </a>
+
+      <div className="flex items-center gap-4">
+        <div className=" h-[1.125rem] w-5">
+          <img
+            className="h-full w-full object-cover"
+            src={sunIcon}
+            alt="sun icon"
+          />
+        </div>
+        <label className="relative inline-block h-6 w-12">
+          <input
+            className="peer h-0 w-0 opacity-0"
+            type="checkbox"
+            checked={isDarkMode}
+            onChange={toggleDarkMode}
+          />
+          <span className="absolute bottom-0 left-0 right-0 top-0 cursor-pointer rounded-xl bg-white before:absolute before:left-[5px] before:top-[5px] before:h-[0.875rem] before:w-[0.875rem] before:rounded-[50%] before:bg-violet before:transition-transform before:content-[''] peer-checked:before:translate-x-[24px]" />
+        </label>
+        <div className="h-3 w-3">
+          <img
+            className="h-full w-full object-cover"
+            src={moonIcon}
+            alt="moon icon"
+          />
+        </div>
+      </div>
     </header>
   );
 };
